963688cbbc8f1293dfbd51fbdec944b40720875c
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-09 17:22:03 -0500

    working on data structure

--- scripts/main.js
+++ scripts/main.js
@@ -30,17 +30,53 @@ async function plot(pointsArray, featureName) {
     )
 }
 
+function normalize(tensor) {
+    const min = tensor.min()
+    const max = tensor.max()
+    const normalizedTensor = tensor.sub(min).div(max.sub(min))
+    return {
+        tensor: normalizedTensor,
+        min,
+        max
+    }
+}
+
+function denormalize(tensor, min, max) {
+    const denormalizedTensor = tensor.mul(max.sub(min)).add(min)
+    return denormalizedTensor
+}
+
 async function run () {
-    const houseSalesDataSet = tf.data.csv("http://127.0.0.1:5500/kc_house_data.csv")
-    const sampleDataSet = houseSalesDataSet.take(10)
-    const dataArray = await sampleDataSet.toArray()
-    console.log(dataArray)
+    const houseSalesDataSet = tf.data.csv("http://127.0.0.1:5500//kc_house_data.csv")
+    // const sampleDataSet = houseSalesDataSet.take(10)
+    // const dataArray = await sampleDataSet.toArray()
+    // console.log(dataArray)
 
     const points = houseSalesDataSet.map(record => ({
         x: record.sqft_living,
         y: record.price
-}))
-plot(await points.toArray(), "Square feet")
+    }))
+    plot(await points.toArray(), "Square feet")
+
+    // features (inputs) 
+    const featureValues = await points.map(p => p.x).toArray()
+    const featureTensor = tf.tensor2d(featureValues, [featureValues.length, 1])
+
+    //labels (outputs)
+    const labelValues = await points.map(p => p.y).toArray()
+    const labelTensor = tf.tensor2d(labelValues, [labelValues.length, 1])
+
+    featureTensor.print()
+    labelTensor.print()
+
+    // const normalizedFeature = normalize(featureTensor)
+    // const normalizedLabel = normalize(labelTensor)
+
+    // normalizedFeature.tensor.print()
+    // normalizedLabel.tensor.print()
+
+    // denormalize(normalizedFeature.tensor, normalizedFeature.min, normalizedFeature.max).print()
+
 }
 
 run()
